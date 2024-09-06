ba292cabf695e6e8faff3eaa69a0740f3b632f9d
Niels Gandraß <niels@gandrass.de>
2024-09-06 13:14:34 +0200

    Remove second globals object from main.cpp (real instance is inside FSM class)

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,6 @@ constexpr unsigned int INTERVAL_BATTERY_CHECK = 60000;
 // Initalizing the board with a brightness above 49 can cause stability issues!
 constexpr uint8_t ABSOLUTE_MAX_BRIGHTNESS = 45;
 FSM fsm(10);
-FSMGlobals globals;
 EFBoardPowerState pwrstate;
 
 // Task counters
@@ -218,7 +217,7 @@ void setup() {
     // Init board
     EFBoard.setup();
     EFLed.init(ABSOLUTE_MAX_BRIGHTNESS);
-    EFLed.setBrightnessPercent(globals.ledBrightnessPercent);
+    EFLed.setBrightnessPercent(40);
     bootupAnimation();
     
     // Touchy stuff
